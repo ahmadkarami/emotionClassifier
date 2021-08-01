532271bc15701a847adb4c991ad37c0be7e466e3
ahmad <ahmadkarami73@gmail.com>
2021-08-01 13:26:44 +0430

    Add files via upload

--- data_preparation.py
+++ data_preparation.py
@@ -5,11 +5,12 @@ import numpy as np
 import shutil as sh
 import librosa.display
 import matplotlib as plt
+import scipy.io.wavfile
 
 TEST_DIR = "test"
 TRAIN_DIR = "train"
-WAV_DATASET_DIR = "Ravdess"
-SPECTROGRAM_DATASET_DIR = "spectrogram_data_set"
+VAL_DIR = "validation"
+RAVDESS_DIR = "Ravdess"
 
 EMOTIONS = {
     1:'neutral', 
@@ -23,10 +24,70 @@ EMOTIONS = {
 }
 
 
-def create_spectrogram():
-    
-     for subdir, dirs, files in os.walk(WAV_DATASET_DIR):
-           
+# ----------------------- This section will be added soon ------------------
+# =============================================================================
+# def split_TESS_audio(path):    
+#      key_list = list(TESS_EMOTIONS.keys())
+#      val_list = list(TESS_EMOTIONS.values())
+#      if not os.path.exists(SPLITTED_DATASET):
+#        os.mkdir(SPLITTED_DATASET)
+#        for emo in EMOTIONS:
+#             if not os.path.exists(SPLITTED_DATASET + "/" + EMOTIONS[emo]):
+#                 os.mkdir(SPLITTED_DATASET + "/" + EMOTIONS[emo])        
+#        for subdir, dirs, files in os.walk(path):
+#         for file in files:
+#             position = val_list.index(file.split('.')[0].split('_')[2])
+#             try:
+#                 src = os.path.join(subdir + "/", file)
+#                 ind = key_list[position]
+#                 dst = os.path.join(SPLITTED_DATASET + "/" + EMOTIONS[ind] + "/", file)
+#                 sh.copyfile(src, dst)                
+#             except ValueError as err:
+#                 print(err)
+#                 continue
+# =============================================================================
+# ----------------------- This section will be added soon ------------------
+
+
+def split_RAVDESS_audio(path):       
+       for emo in EMOTIONS:
+            if not os.path.exists(TRAIN_DIR + "/" + EMOTIONS[emo]):
+                os.mkdir(TRAIN_DIR + "/" + EMOTIONS[emo])        
+       for subdir, dirs, files in os.walk(path):
+        for file in files:
+            try:
+                src = os.path.join(subdir + "/", file)
+                ind = int(file.split('.')[0].split('-')[2])
+                dst = os.path.join(TRAIN_DIR + "/" + EMOTIONS[ind] + "/", file)
+                sh.copyfile(src, dst)                
+            except ValueError as err:
+                print(err)
+                continue
+
+def data_augmentation(path, noise= True, stretch= False, shift= True, pitch= True):
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            clip, sample_rate = librosa.load(subdir+'/'+file, sr=None)            
+            if "aug" not in file: 
+                if noise:
+                    clip, sample_rate = librosa.load(subdir+'/'+file, sr=None)
+                    noise_amp = 0.035*np.random.uniform()*np.amax(clip)
+                    data = clip + noise_amp*np.random.normal(size=clip.shape[0])
+                    scipy.io.wavfile.write(subdir + "/aug_noise_" + file, 16000, data)                
+                if stretch:
+                    clip, sample_rate = librosa.load(subdir+'/'+file, sr=None)
+                    data = librosa.effects.time_stretch(clip, sample_rate)
+                    scipy.io.wavfile.write(subdir + "/aug_stretch_" + file, 16000, data)                    
+                if shift:
+                    shift_range = int(np.random.uniform(low=-5, high = 5)*1000)
+                    data = np.roll(clip, shift_range)
+                    scipy.io.wavfile.write(subdir + "/aug_shift_" + file, 16000, data)                    
+                if pitch:
+                    data = librosa.effects.pitch_shift(clip, sample_rate, 0.7)
+                    scipy.io.wavfile.write(subdir + "/aug_pitch_" + file, 16000, data)
+
+def create_spectrogram(path):    
+     for subdir, dirs, files in os.walk(path):           
            for file in files:
                try:
                     plt.interactive(False)
@@ -38,63 +99,32 @@ def create_spectrogram():
                     ax.set_frame_on(False)
                     S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
                     librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-                    filename  = SPECTROGRAM_DATASET_DIR + '/' + file.split('.')[0] + '.jpg'
+                    filename  = subdir + '/' + file.split('.')[0] + '.jpg'
                     plt.pyplot.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
                     plt.pyplot.close()    
                     fig.clf()
                     plt.pyplot.close(fig)
                     plt.pyplot.close('all')
                     del filename,clip,sample_rate,fig,ax,S
+                    os.remove(subdir + '/' + file)
                except ValueError as err:
                    print(err)
                    continue
 
-
-def split_data_train():
-    
-    if not os.path.exists(TRAIN_DIR):
-        os.mkdir(TRAIN_DIR)
-
-
-        for emo in EMOTIONS:
-            if not os.path.exists(TRAIN_DIR + "/" + EMOTIONS[emo]):
-                os.mkdir(TRAIN_DIR + "/" + EMOTIONS[emo])
-        
-        for subdir, dirs, files in os.walk(SPECTROGRAM_DATASET_DIR):
-            for file in files:
-                try:
-                    src = os.path.join(SPECTROGRAM_DATASET_DIR + "/", file)
-                    ind = int(file.split('.')[0].split('-')[2])
-                    dst = os.path.join(TRAIN_DIR + "/" + EMOTIONS[ind] + "/", file)
-                    sh.move(src, dst)
-                    
-                except ValueError as err:
-                    print(err)
-                    continue
-
-
 def split_data_test():
     if not os.path.exists(TEST_DIR):
         os.mkdir(TEST_DIR)
-
-
         for emo in EMOTIONS:
             if not os.path.exists(TEST_DIR + "/" + EMOTIONS[emo]):
-                os.mkdir(TEST_DIR + "/" + EMOTIONS[emo])
-        
-        
-        for subdir, dirs, files in os.walk(TRAIN_DIR):
-            
+                os.mkdir(TEST_DIR + "/" + EMOTIONS[emo])            
+        for subdir, dirs, files in os.walk(TRAIN_DIR):            
             x = len(files)
-            no = len(files) * 0.3
+            no = len(files) * 0.15
             if x != 0:
                 random_index = random.sample(range(x),int(no))
                 random_file = [files[i] for i in random_index]
-                
                 for file in random_file:
-                    
                     try:
-                        
                         src = os.path.join(TRAIN_DIR + "/" +  subdir.split('\\')[1] + "/", file)
                         dst = os.path.join(TEST_DIR + "/" + subdir.split('\\')[1] + "/", file)
                         sh.move(src, dst)
@@ -102,11 +132,32 @@ def split_data_test():
                         print(err)
                         continue
 
+def split_data_val():
+    if not os.path.exists(VAL_DIR):
+        os.mkdir(VAL_DIR)
+        for emo in EMOTIONS:
+            if not os.path.exists(VAL_DIR + "/" + EMOTIONS[emo]):
+                os.mkdir(VAL_DIR + "/" + EMOTIONS[emo])            
+        for subdir, dirs, files in os.walk(TRAIN_DIR):            
+            x = len(files)
+            no = len(files) * 0.15
+            if x != 0:
+                random_index = random.sample(range(x),int(no))
+                random_file = [files[i] for i in random_index]                
+                for file in random_file:                    
+                    try:                        
+                        src = os.path.join(TRAIN_DIR + "/" +  subdir.split('\\')[1] + "/", file)
+                        dst = os.path.join(VAL_DIR + "/" + subdir.split('\\')[1] + "/", file)
+                        sh.move(src, dst)
+                    except ValueError as err:
+                        print(err)
+                        continue
 
-if not os.path.exists(SPECTROGRAM_DATASET_DIR):
-       os.mkdir(SPECTROGRAM_DATASET_DIR)
-       create_spectrogram()
-       split_data_train()
-       split_data_test()
-      
-    
+def preparation():
+    if not os.path.exists(TRAIN_DIR):
+        os.mkdir(TRAIN_DIR)
+        split_RAVDESS_audio(RAVDESS_DIR)
+        data_augmentation(TRAIN_DIR)
+        create_spectrogram(TRAIN_DIR)
+        split_data_test()
+        split_data_val()
--- emotion_classifier.py
+++ emotion_classifier.py
@@ -5,65 +5,72 @@ from keras import optimizers
 from keras.models import Sequential, Model
 from keras import regularizers, optimizers
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, Conv2D, MaxPooling2D
+from keras.layers import LSTM, Dense, Activation, Flatten, Dropout, BatchNormalization, Conv2D, MaxPooling2D
+from data_preparation import preparation
+
+preparation()
 
 train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory("train/",target_size=(150, 150),batch_size=20,class_mode='categorical')
-validation_generator = test_datagen.flow_from_directory("test/",target_size=(150, 150),batch_size=20,class_mode='categorical')
+#flow_from_directory() read the images from a big folders containing images.
+train_generator = train_datagen.flow_from_directory("train/",target_size=(150, 150),batch_size=20, class_mode='categorical')
+validation_generator = test_datagen.flow_from_directory("validation/",target_size=(150, 150),batch_size=20, class_mode='categorical')
 
 model = models.Sequential()
 
 model.add(layers.Conv2D(32, (3, 3), activation='relu',input_shape=(150, 150, 3)))
-model.add(BatchNormalization())
+model.add(BatchNormalization()) # baraye kaheshe zamane train
 model.add(Activation('elu'))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3)) # baraye jelogiri az overfit
 
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(BatchNormalization())
 model.add(Activation('elu'))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3)) # baraye jelogiri az overfit
 
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(BatchNormalization())
 model.add(Activation('elu'))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3)) # baraye jelogiri az overfit
 
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(BatchNormalization())
 model.add(Activation('elu'))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3)) # baraye jelogiri az overfit
 
 model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
 
 model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(8, activation='sigmoid'))
 
 print(model.summary())
 
-model.compile(loss='binary_crossentropy',optimizer= "adam",metrics=['acc'])
-model.fit(train_generator,steps_per_epoch=15,epochs=100,validation_data=validation_generator,validation_steps=10)
+#model.compile(loss='kullback_leibler_divergence', optimizer= "adam", metrics=['acc'])
+model.compile(loss='binary_crossentropy', optimizer= "nadam", metrics=['acc'])
+
+STEP_TRAIN = train_generator.n // train_generator.batch_size
+STEP_VAL = validation_generator.n // validation_generator.batch_size
+
+model.fit(train_generator,steps_per_epoch= STEP_TRAIN, epochs = 20
+          ,validation_data = validation_generator, validation_steps= STEP_VAL)
 model.save('emotionRecognitionClassifier.h5')
 
 eval_datagen = ImageDataGenerator(rescale=1./255)
-eval_generator = eval_datagen.flow_from_directory("test/",target_size=(150, 150),batch_size=20,class_mode='categorical')
+eval_generator = eval_datagen.flow_from_directory("test/",target_size=(150, 150),batch_size=20, class_mode='categorical')
 eval_generator.reset()    
-pred = model.predict_generator(eval_generator,5,verbose=1)
+pred = model.predict(eval_generator,5,verbose=1)
 
-STEP_SIZE_VALID = eval_generator.n//eval_generator.batch_size
+STEP_SIZE_VALID = eval_generator.n // eval_generator.batch_size
+
 p = model.evaluate_generator(generator=eval_generator, steps=STEP_SIZE_VALID)
 
 print("Predictions is finished")
 print("loss => " ,p[0])
 print("accuracy => " ,p[1])
-
-
-
-
-
-
-
 
 
