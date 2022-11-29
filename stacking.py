7c04bf4ceaae7bd7cf22599d63e1c388127219a6
ahmad <ahmadkarami73@gmail.com>
2022-11-29 15:15:19 +0330

    impoer library

--- stacking.py
+++ stacking.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from keras.preprocessing.image import load_img
